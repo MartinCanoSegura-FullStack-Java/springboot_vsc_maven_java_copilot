1697ea3bbb0393e38cb88c965ca3d62522f3865c
MecanoCS <119688437+MartinCanoSegura-FullStack-Java@users.noreply.github.com>
2023-04-27 22:21:04 -0600

    Update SpringbootVscMavenJavaCopilotApplication.java
    Cambio desde github

--- src/main/java/com/mx/mcs/springboot_vsc_maven_java_copilot/SpringbootVscMavenJavaCopilotApplication.java
+++ src/main/java/com/mx/mcs/springboot_vsc_maven_java_copilot/SpringbootVscMavenJavaCopilotApplication.java
@@ -5,29 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootVscMavenJavaCopilotApplication {
-
-	public void factorial(int n) {
-		int fact = 1;
-		if(n < 0) {
-			System.out.println("Number should be non-negative.");
-		}
-		if(n == 0) {
-			System.out.println("Factorial of 0 is 1.");
-		}
-		else {
-			for (int i = 1; i <= n; i++) {
-				fact = fact * i;
-			}
-		}
-		System.out.println("Factorial of " + n + " is: " + fact);
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootVscMavenJavaCopilotApplication.class, args);
-
-		SpringbootVscMavenJavaCopilotApplication app = new SpringbootVscMavenJavaCopilotApplication();
-		app.factorial(5);
 	}
 
 }

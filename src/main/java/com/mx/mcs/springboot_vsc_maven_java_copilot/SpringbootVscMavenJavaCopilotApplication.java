e56a16975893edc49d86d1cd5f0f85dc344c6b2c
Martin Cano Segura <mecano_mcs@hotmail.com>
2023-04-27 22:54:27 -0600

    Segundo Commit - Agrga factorial

--- src/main/java/com/mx/mcs/springboot_vsc_maven_java_copilot/SpringbootVscMavenJavaCopilotApplication.java
+++ src/main/java/com/mx/mcs/springboot_vsc_maven_java_copilot/SpringbootVscMavenJavaCopilotApplication.java
@@ -5,8 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootVscMavenJavaCopilotApplication {
+
+	public void factorial(int n) {
+		int fact = 1;
+		if (n == 0) {
+			System.out.println("Factorial of " + n + " is: " + fact);
+		}
+		if (n < 0) {
+			System.out.println("Number should be non-negative.");
+		}
+		else {
+			for (int i = 1; i <= n; i++) {
+				fact = fact * i;
+			}
+		}
+		System.out.println("Factorial of " + n + " is: " + fact);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootVscMavenJavaCopilotApplication.class, args);
+		SpringbootVscMavenJavaCopilotApplication obj = new SpringbootVscMavenJavaCopilotApplication();
+		obj.factorial(5);
 	}
 
 }
